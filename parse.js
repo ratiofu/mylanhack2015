d3defeb80a514525ce1d76ec7dab0ce1b4fef2d4
Thomas Jung <thomas.jung@jivesoftware.com>
2015-02-28 14:51:17 -0500

    tasks

--- parse.js
+++ parse.js
@@ -1,3 +1,12 @@
+var parseString = require('xml2js').parseString,
+    fs = require('fs')
 
-// var xml = require('xml')
-console.log('hello world')
+fs.readFile('../Test Project 1 XML.xml', 'utf8', function(err, data) {
+  if (err) throw err
+  parseString(data, function (err, parsedData) {
+    if (err) throw err
+    console.log(parsedData.Project.Tasks[0].Task)
+    // console.log(parsedData.Project.Title)
+    // result.Project.Tasks.forEach(function (task))
+  })
+})
